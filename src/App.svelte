a3150818f4d6c8782569876df8c378dc94868bd4
Daniel Hoeflehner <daniel.hoeflehner@outlook.com>
2020-02-09 11:47:38 +0100

    Show when coffee is delivery cost free

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,19 @@
 <script>
-  let numPersons = 1;
-  let numCoffees = 1;
+  let countP = 1;
+  let countC = 1;
   let type = "7.5";
   let days = "14";
 
-  $: sum14 = numPersons * numCoffees * parseFloat(type) * 14;
-  $: sum21 = numPersons * numCoffees * parseFloat(type) * 21;
-  $: sum28 = numPersons * numCoffees * parseFloat(type) * 28;
-  $: sum42 = numPersons * numCoffees * parseFloat(type) * 42;
-  $: sum56 = numPersons * numCoffees * parseFloat(type) * 56;
+  $: sum14 = ((countP * countC * parseFloat(type) * 14) / 1000).toFixed(2);
+  $: noCost14 = sum14 >= 2.0;
+  $: sum21 = ((countP * countC * parseFloat(type) * 21) / 1000).toFixed(2);
+  $: noCost21 = sum21 >= 2.0;
+  $: sum28 = ((countP * countC * parseFloat(type) * 28) / 1000).toFixed(2);
+  $: noCost28 = sum28 >= 2.0;
+  $: sum42 = ((countP * countC * parseFloat(type) * 42) / 1000).toFixed(2);
+  $: noCost42 = sum42 >= 2.0;
+  $: sum56 = ((countP * countC * parseFloat(type) * 56) / 1000).toFixed(2);
+  $: noCost56 = sum56 >= 2.0;
 </script>
 
 <div class="px-4 py-8">
@@ -21,11 +26,10 @@
         Anzahl der Kaffeetrinker
       </label>
       <input
-        bind:value="{numPersons}"
+        bind:value="{countP}"
         class="appearance-none block w-full bg-white text-gray-700 border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-        id="grid-first-name"
-        min="0"
-        placeholder="Jane"
+        id="count-persons"
+        min="1"
         step="1"
         type="number"
       />
@@ -38,15 +42,16 @@
         Anzahl Kaffees / Person / Tag
       </label>
       <input
+        bind:value="{countC}"
         class="appearance-none block w-full bg-white text-gray-700 border border-gray-400 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-        id="grid-last-name"
+        id="count-coffees"
         type="number"
-        placeholder="Doe"
-        bind:value="{numCoffees}"
+        min="1"
+        step="1"
       />
     </div>
   </div>
-  <div class="flex flex-wrap">
+  <div class="flex flex-wrap mb-8">
     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <label
         class="block tracking-wide text-gray-700 text-sm mb-2"
@@ -57,7 +62,7 @@
       <div class="relative">
         <select
           class="block appearance-none w-full bg-white border border-gray-400 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-          id="grid-state"
+          id="type"
           bind:value="{type}"
         >
           <option value="10.5">Vollautomat</option>
@@ -81,36 +86,78 @@
       </div>
     </div>
   </div>
-  <div class="pt-8">
-    <table class="table-fixed">
-      <thead>
-        <tr>
-          <th class="w-3/4 px-4 py-2">Anzahl der Wochen</th>
-          <th class="w-1/4 px-4 py-2">Kaffee</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th class="w-3/4 px-4 py-2">in 2 Wochen</th>
-          <th class="w-1/4 px-4 py-2">{sum14}g</th>
-        </tr>
-        <tr>
-          <th class="w-3/4 px-4 py-2">in 3 Wochen</th>
-          <th class="w-1/4 px-4 py-2">{sum21}g</th>
-        </tr>
-        <tr>
-          <th class="w-3/4 px-4 py-2">in 4 Wochen</th>
-          <th class="w-1/4 px-4 py-2">{sum28}g</th>
-        </tr>
-        <tr>
-          <th class="w-3/4 px-4 py-2">in 6 Wochen</th>
-          <th class="w-1/4 px-4 py-2">{sum42}g</th>
-        </tr>
-        <tr>
-          <th class="w-3/4 px-4 py-2">in 8 Wochen</th>
-          <th class="w-1/4 px-4 py-2">{sum56}g</th>
-        </tr>
-      </tbody>
-    </table>
+  <div class="px-3">
+    <div class="flex">
+      <label class="tracking-wide text-gray-700 text-sm mb-2" for="result">
+        Lieferinterval
+      </label>
+      <span class="w-full text-gray-700 text-sm text-right">
+        <span
+          class="bg-bd-brown"
+          style="border-radius: 6px; display: inline-block; height: 12px; width: 12px; position: relative; top: 2px;"
+        ></span>
+        = versandkostenfrei*
+      </span>
+    </div>
+    <div
+      id="result"
+      class="flex flex-col bg-white border border-gray-400 rounded"
+    >
+      <div class="flex">
+        <span class="w-2/3 px-4 py-2 text-gray-700 font-light"
+          >alle <span class="font-normal">2</span> Wochen</span
+        >
+        <span class="w-1/3 px-4 py-2 text-right"
+          ><span
+            class="{noCost14 ? 'bg-bd-brown text-white p-1 rounded' : 'p-1'}"
+            >{sum14} kg</span
+          ></span
+        >
+      </div>
+      <div class="flex bg-gray-200">
+        <span class="w-2/3 px-4 py-2 text-gray-700 font-light"
+          >alle <span class="font-normal">3</span> Wochen</span
+        >
+        <span class="w-1/3 px-4 py-2 text-right"
+          ><span
+            class="{noCost21 ? 'bg-bd-brown text-white p-1 rounded' : 'p-1'}"
+            >{sum21} kg</span
+          ></span
+        >
+      </div>
+      <div class="flex">
+        <span class="w-2/3 px-4 py-2 text-gray-700 font-light"
+          >alle <span class="font-normal">4</span> Wochen</span
+        >
+        <span class="w-1/3 px-4 py-2 text-right"
+          ><span
+            class="{noCost28 ? 'bg-bd-brown text-white p-1 rounded' : 'p-1'}"
+            >{sum28} kg</span
+          ></span
+        >
+      </div>
+      <div class="flex bg-gray-200">
+        <span class="w-2/3 px-4 py-2 text-gray-700 font-light"
+          >alle <span class="font-normal">6</span> Wochen</span
+        >
+        <span class="w-1/3 px-4 py-2 text-right"
+          ><span
+            class="{noCost42 ? 'bg-bd-brown text-white p-1 rounded' : 'p-1'}"
+            >{sum42} kg</span
+          ></span
+        >
+      </div>
+      <div class="flex">
+        <span class="w-2/3 px-4 py-2 text-gray-700 font-light"
+          >alle <span class="font-normal">8</span> Wochen</span
+        >
+        <span class="w-1/3 px-4 py-2 text-right"
+          ><span
+            class="{noCost56 ? 'bg-bd-brown text-white p-1 rounded' : 'p-1'}"
+            >{sum56} kg</span
+          ></span
+        >
+      </div>
+    </div>
   </div>
 </div>
